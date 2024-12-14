70b07980d6a8b5699d8ae37f8955dbf4dea8f79d
Elmar <elmarosmanli765@gmail.com>
2024-12-15 02:07:32 +0400

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -32,6 +32,39 @@ const featuredProducts = [
         image: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/07/f8/88/d2/getlstd-property-photo.jpg?w=1200&h=-1&s=1',
         isPremium: true,
     },
+
+/*sile bilersen 5 den 8 e alem deyse bir birine ui da */
+    
+    {
+        id: '5',
+        name: 'Araz Supermarket',
+        price: 149999,
+        image: 'https://arazmarket.az/site/assets/files/1039/araz-market-haqqimizda.570x475.webp',
+        isPremium: true,
+        isStore: true,
+    },
+    {
+        id: '6',
+        name: 'Playstation Zal',
+        price: 16500,
+        image: 'https://i.pinimg.com/736x/56/64/8e/56648ec056779db54814327d65a1ae8e.jpg',
+        isPremium: true,
+    },
+    {
+        id: '7',
+        name: '50 Qəpik Avtoyuma',
+        price: 99000,
+        image: 'https://avatars.mds.yandex.net/get-altay/9916116/2a00000189a2fbbada06c1f8af78be68966d/orig',
+        isPremium: true,
+        isStore: true,
+    },
+    {
+        id: '8',
+        name: 'AREA Restoran',
+        price: 87000,
+        image: 'https://area.az/frontend/web/uploads/property/04f5a53f-728c-4f75-bb05-aba28aae3a73/04f5a53f-728c-4f75-bb05-aba28aae3a73.jpg',
+        isPremium: true,
+    },
 ];
 
 export default function Home() {
