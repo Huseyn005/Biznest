37052eb58180094a86d2c2bfd6bf66537f4f629a
Huseyn <77039657+Huseyn005@users.noreply.github.com>
2024-12-15 03:16:39 +0400

    Update CategoryGrid.tsx

--- components/CategoryGrid.tsx
+++ components/CategoryGrid.tsx
@@ -5,7 +5,7 @@ const categories = [
   { id: 1, name: 'Kafe və Restoranlar', icon: "https://cdn-icons-png.flaticon.com/512/9620/9620771.png"  },
   { id: 2, name: 'Gözəllik Salonları', icon: 'https://cdn-icons-png.flaticon.com/512/3791/3791216.png' },
   { id: 3, name: 'Fitness & SPA Mərkəzləri', icon: "https://cdn-icons-png.flaticon.com/512/13632/13632470.png" },
-  { id: 4, name: 'Təlim-Tədris mərkəzləri', icon: "   https://cdn-icons-png.flaticon.com/512/9733/9733500.png " },
+  { id: 4, name: 'Təlim-Tədris mərkəzləri', icon: "https://cdn-icons-png.flaticon.com/512/9733/9733500.png" },
   { id: 5, name: 'Tibb mərkəzləri və klinikalar', icon: "https://cdn-icons-png.flaticon.com/512/4320/4320371.png" },
   { id: 6, name: 'Mədəniyyət obyektləri', icon: "https://cdn-icons-png.flaticon.com/512/10987/10987278.png " },
   { id: 7, name: 'Hotellər və Turizm biznesləri', icon: "https://cdn-icons-png.flaticon.com/512/3009/3009487.png " },
