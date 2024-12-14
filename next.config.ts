9e91ee1e1b6969a9b4f694141082a786e3deb50b
Huseyn005 <huseyn.mammadli.005@gmail.com>
2024-12-15 02:17:26 +0400

    Add flaticon to remotePatterns in next.config.js

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,38 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  images: {
-    domains: ['cdn-icons-png.flaticon.com'], // Add the external image domain here
-  },
-  // Add other config options as needed
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'bzns.az',
+            },
+            {
+                protocol: 'https',
+                hostname: 'avatars.mds.yandex.net',
+            },
+            {
+                protocol: 'https',
+                hostname: 'dynamic-media-cdn.tripadvisor.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'arazmarket.az',
+            },
+            {
+                protocol: 'https',
+                hostname: 'i.pinimg.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'area.az',
+            },
+            {
+                protocol: 'https',
+                hostname: 'cdn-icons-png.flaticon.com', // Add flaticon domain here
+            },
+        ],
+    },
 };
 
 export default nextConfig;
