6a16121115aae8eb6950ecb7ba5399da4cc98de2
Shamil116 <118134066+Shamil116@users.noreply.github.com>
2024-12-15 11:05:14 +0400

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -27,7 +27,7 @@ export default function Header() {
               <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 h-4 w-4" />
               <Input 
                 className="w-full pl-10" 
-                placeholder="Əşya və ya xidmət axtarışı"
+                placeholder="Biznes axtarışı"
               />
             </div>
           </div>
